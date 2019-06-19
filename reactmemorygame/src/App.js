e51a96efbc25368629d5c4f3c090d0da1cd6802f
Grimble <grimblelewisthethird@gmail.com>
2019-06-19 17:33:29 -0400

    added a navbar component, score isn't functioning yet and I'd still like to work on its scaling but it looks okay to start with, moving on to the header

--- reactmemorygame/src/App.js
+++ reactmemorygame/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import Wrapper from "./components/Wrapper";
 import ImageCard from "./components/ImageCard";
 import images from "./images.json";
+import Navbar from "./components/Navbar";
 
 function App() {
-    return (
+  return (
+    <>
+      <Navbar />
       <Wrapper>
-        <ImageCard
-        image={images[0].images} 
-        />
-    </Wrapper>
+        <ImageCard image={images[0].image} />
+      </Wrapper>
+    </>
   );
 }
 
---
+++ reactmemorygame/src/components/Navbar/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./style.css";
+
+function Navbar() {
+  return (
+    <>
+ 
+
+      <nav className="navbar sticky-top">
+        <div className="container-fluid">
+        <ul className="nav navbar-nav">
+
+          <li className="navbar-brand">
+            <a href="/">Memory Game</a>
+          </li>
+
+
+          <li className="instructions">Click an image to begin!</li>
+
+
+          <li className="score">Score: 0 | Top Score: 0</li>
+
+        </ul>
+        </div>
+      </nav>
+
+    
+    </>
+  );
+}
+
+export default Navbar;
