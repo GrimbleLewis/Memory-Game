910123ec40f409f506a182fe9fb4fd965cfd1322
Grimble <grimblelewisthethird@gmail.com>
2019-06-19 19:50:18 -0400

    added the rest of the cards with images of futurama characters, need to figure out how to display all of them without having 12 <ImageCards/> on the App.js

--- reactmemorygame/src/App.js
+++ reactmemorygame/src/App.js
@@ -13,6 +13,18 @@ function App() {
         <Header />
         <Wrapper>
         <ImageCard image={images[0].image} />
+        <ImageCard image={images[1].image} />
+        <ImageCard image={images[2].image} />
+        <ImageCard image={images[3].image} />
+        <ImageCard image={images[4].image} />
+        <ImageCard image={images[5].image} />
+        <ImageCard image={images[6].image} />
+        <ImageCard image={images[7].image} />
+        <ImageCard image={images[8].image} />
+        <ImageCard image={images[9].image} />
+        <ImageCard image={images[10].image} />
+        <ImageCard image={images[11].image} />
+
         </Wrapper>
     </>
   );
