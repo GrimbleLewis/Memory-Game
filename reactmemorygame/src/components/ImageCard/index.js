69020326098ba97b3acd5a7a0d442edea8207e60
Grimble <grimblelewisthethird@gmail.com>
2019-06-20 17:18:10 -0400

    added a score component, nothing functioning yet

--- reactmemorygame/src/components/ImageCard/index.js
+++ reactmemorygame/src/components/ImageCard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Counter from "../ScoreCounter"
 import "./style.css";
 
 
@@ -12,5 +13,6 @@ function ImageCard(props) {
   </div>
   )
 }
+
 
 export default ImageCard;
---
+++ reactmemorygame/src/components/ScoreCounter/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Counter extends React.Component {
+
+    state = {
+      score: 0
+    }
+  
+ 
+  
+  handleScore = () => {
+    
+  }
+
+};
+
+
+
+export default Counter;
