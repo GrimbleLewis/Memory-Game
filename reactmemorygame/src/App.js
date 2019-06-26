923aee618860b02e3f136894e42ac9327aba1a9b
Grimble <grimblelewisthethird@gmail.com>
2019-06-26 14:56:45 -0400

    implemented the shuffle array function into the onClick of the images, currently it only increments the score by 1 any time you click any image, but the shuffle works :)

--- reactmemorygame/src/App.js
+++ reactmemorygame/src/App.js
@@ -7,38 +7,46 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 class App extends Component {
-  state = {
-    images,
-    score: 0,
-    topscore: 0
-  };
+    state = {
+      images,
+      score: 0,
+      topscore: 0
+    };
 
 
   shuffleArray = array => {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-  
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
+
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
+  };
+
+  handleOnClick = id => {
+    let clicked = this.state.images.filter(image => image.id === id)[0];
+    console.log(clicked);
+    this.shuffleArray(images);
+    this.setState({
+      score: this.state.score + 1
+    });
   };
 
   render() {
     return (
       <>
-        <Navbar 
-        score={this.state.score}
-        topscore={this.state.topscore}/>
+        <Navbar score={this.state.score} topscore={this.state.topscore} />
 
         <Header />
         <Wrapper>
@@ -48,7 +56,7 @@ class App extends Component {
               key={card.id}
               name={card.name}
               image={card.image}
-              shuffleArray={this.shuffleArray}
+              handleOnClick={this.handleOnClick}
             />
           ))}
         </Wrapper>
