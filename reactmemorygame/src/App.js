9889f09ec2b58a2f66da0e8f6502cded9c6b88b1
Grimble <grimblelewisthethird@gmail.com>
2019-06-27 17:14:06 -0400

    added the logic to keep track of the score and set the topscore, need to add some more detailed comments and add to the readme, then deploy

--- reactmemorygame/src/App.js
+++ reactmemorygame/src/App.js
@@ -7,12 +7,16 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 class App extends Component {
-    state = {
-      images,
-      score: 0,
-      topscore: 0
-    };
+  constructor(props){
+    super(props);
+  this.state = {
+    images,
+    score: 0,
+    topscore: 0
+  };
 
+  this.handleOnClick = this.handleOnClick.bind(this);
+} 
 
   shuffleArray = array => {
     let currentIndex = array.length,
@@ -35,12 +39,51 @@ class App extends Component {
   };
 
   handleOnClick = id => {
-    let clicked = this.state.images.filter(image => image.id === id)[0];
-    console.log(clicked);
-    this.shuffleArray(images);
-    this.setState({
-      score: this.state.score + 1
-    });
+    //grabs the id of the image object that was clicked
+    let clickedImage = this.state.images.filter(image => image.id === id)[0];
+
+    let alreadyBeenClicked = this.state.images.filter(
+      image => image.clicked === true
+    );
+    console.log(clickedImage);
+
+    if (!clickedImage.clicked) {
+      clickedImage.clicked = true;
+      this.shuffleArray(images);
+
+      // updates the users score and topscore based on if current score is higher than previous top score
+      this.setState({
+        score: this.state.score + 1,
+        topscore:
+          this.state.score + 1 > this.state.topscore
+            ? this.state.score + 1
+            : this.state.topscore
+      });
+    
+
+    // if the user beats the game, shuffle the images and reset the game by setting all images.clicked back to false
+    if (this.state.score === 11) {
+      this.setState({
+        score: 0
+      });
+
+      this.shuffleArray(images);
+
+      alreadyBeenClicked.forEach(image => {
+        image.clicked = false;
+      });
+    }
+    } else {
+      alreadyBeenClicked.forEach(image => {
+        image.clicked = false;
+      });
+
+      this.shuffleArray(images);
+
+      this.setState({
+        score: 0
+      });
+    }
   };
 
   render() {
