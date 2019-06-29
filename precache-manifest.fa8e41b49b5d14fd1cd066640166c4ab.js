9458b99ab8da85dbf0351454dd52139185c50d34
GrimbleLewis <grimblelewisthethird@gmail.com>
2019-06-29 10:57:30 -0400

    Updates

---
+++ precache-manifest.fa8e41b49b5d14fd1cd066640166c4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afa92efb70d1680d7f355edaa76c4e3",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "460588e488f20061cea9",
+    "url": "/Memory-Game/static/css/main.67d66d7f.chunk.css"
+  },
+  {
+    "revision": "0d2e9063677e12e96e46",
+    "url": "/Memory-Game/static/js/2.bf1050d3.chunk.js"
+  },
+  {
+    "revision": "460588e488f20061cea9",
+    "url": "/Memory-Game/static/js/main.faf12966.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "72478dcd9af0a812c155587b75fb9e24",
+    "url": "/Memory-Game/static/media/background.72478dcd.jpg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/Memory-Game/static/media/react-logo.47ce6e77.png"
+  }
+]);
